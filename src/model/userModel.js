47a94cb9af07f8de64b6f16c989507f7ec1e3699
Clara Dias <112629678+ueeclara@users.noreply.github.com>
2024-01-02 21:18:11 -0300

    conexão banco de dados

--- src/model/userModel.js
+++ src/model/userModel.js
@@ -1,11 +1,11 @@
 import conexaoBancoDeDados from "../database/dbconnect.js";
 
 class UserModel {
-  async salvar(categoria, nome, preço, descrição) {
+  async salvar(categoria, nome, preco, descricao) {
     const conexao = await conexaoBancoDeDados.conectar();
     const comandoSQL =
-      "INSERT INTO users(categoria, nome, preço, descrição) VALUES ($1,$2,$3,$4)";
-    const values = [categoria, nome, preço, descrição];
+      "INSERT INTO users(categoria, nome, preco, descricao) VALUES ($1,$2,$3,$4)";
+    const values = [categoria, nome, preco, descricao];
     return await conexao.query(comandoSQL, values);
   }
   async listasUsers() {
@@ -14,11 +14,11 @@ class UserModel {
     const usuarios = await conexao.query(comandoSQL);
     return usuarios.rows;
   }
-  async atualizar(id, categoria, nome, preço, descrição) {
+  async atualizar(id, categoria, nome, preco, descricao) {
     const conexao = await conexaoBancoDeDados.conectar();
     const comandoSQL =
       "UPDATE users SET categoria=$2, nome=$3, preco=$4, descricao=$5 WHERE id=$1";
-    const values = [id, categoria, nome, preço, descrição];
+    const values = [id, categoria, nome, preco, descricao];
     return await conexao.query(comandoSQL, values);
   }
 
