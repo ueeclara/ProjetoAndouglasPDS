212eb75345b7c318b2f3fd4b6a91580ed2dca4f0
Clara Dias <112629678+ueeclara@users.noreply.github.com>
2024-01-04 17:45:56 -0300

    conexão banco de dados

--- src/database/dbconnect.js
+++ src/database/dbconnect.js
@@ -3,7 +3,7 @@ import pg from "pg";
 async function conectar() {
   const pool = new pg.Pool({
     connectionString:
-      "postgres://aluno_20201214010022:706384@177.136.201.182:5439/temp?schema=aluno_20201214010022",
+      "postgres://aluno_20201214010022:7063@177.136.200.206:5439/temp?schema=aluno_20201214010022",
   });
 
   const conexao = await pool.connect();
